b5de0635325edfb2d0c16130c21125d8ad387ee1
Vishwa <10795270+vishvega@users.noreply.github.com>
2017-10-25 12:51:59 +0530

    Create Index.php

---
+++ HykeCorp/Quickorder/Block/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace HykeCorp\Quickorder\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+	public function getHelloWorldTxt(){
+		return 'Hello world!';
+	}
+	public function isInputEmpty(){
+		//test function x
+	}
+}
